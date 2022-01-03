f47d308f2fc9d0da6803534875264b28f69f9028
eurowondollaryen <sehoakasayho@naver.com>
2022-01-03 23:21:13 +0900

    docker compose

--- docker/nodejs-docker-compose-app/server.js
+++ docker/nodejs-docker-compose-app/server.js
@@ -10,12 +10,13 @@ const client = redis.createClient({
   host: "redis-server",
   port: 6379//redis의 기본 포트
 });
-
+//redis 3.0.2 version에서는 정상 동작하는데, 최신 버전에서는 에러 발생.
 const app = express();
+
 //숫자는 0 부터 시작합니다.
 client.set("number", 0);
 app.get("/", (req, res) => {
-  client("number", (err, number) => {
+  client.get("number", (err, number) => {
     //현재 숫자를 가져온 뒤 1 더한다.
     client.set("number", parseInt(number) + 1);
     res.send("숫자가 1씩 올라갑니다. 숫자:" + number);
